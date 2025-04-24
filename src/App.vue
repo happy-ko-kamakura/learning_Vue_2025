3d8f6adbc01022bd66f357c4ed442c185afd2200
happy-ko-kamakura <sachiko.dev.0811@gmail.com>
2025-04-24 17:12:50 +0900

    first: reactive

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+// const userName = 'Sachi'
+// console.log(userName)
+import { ref, reactive } from 'vue'
+const title = 'happy'
+let price = ref(9.99)
+function increment() {
+  price.value += 1
+  me.age -= 1
+  console.log(price)
+}
+
+const info = ref({
+  students: 1000,
+  rating: 4,
+})
+console.log(info.value.students)
+
+const me = reactive({
+  name: 'happy',
+  age: 29,
+}) //引数にリアクティブにしたいオブジェクトを入れる
+console.log(me)
+</script>
+<template>
+  <h1>Title: {{ title }}</h1>
+  <h3>Price: {{ price }}</h3>
+  <h3>Info: {{ info.students }}</h3>
+  <h3>Me: {{ me.age }}</h3>
+  <button @click="increment">click</button>
+</template>
+<style>
+h1 {
+  color: red;
+}
+</style>
